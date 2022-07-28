8b87875a4940a6a6d70e4448cdba472c648e57ff
Abhishek Kumar <akbittu@gmail.com>
2022-07-29 02:05:48 +0530

    App is ready

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # crond
-A cron daemon CLI based on node-cron
+A cron daemon CLI leveraging node-cron module to schedule the task. 
+
+## Prerequisite
+
+Make sure you have [node.js](https://nodejs.org/) installed at your system.
+
+## Install
+
+You can install the app via npm
+
+```
+$ npm install node-cron-cli
+```
+
+After installation, check the app accessibility
+
+```
+$ crond --version
+```
+
+## Usage
+
+To understand the usage, see below
+
+```
+$ crond --help 
+ Cron daemon (crond)
+
+ Syntax:
+  crond [option]
+  crond [expression] [command]
+
+ Options:
+  -h  --help        Show help menu
+  -v  --version     Show version info
+
+ Usage:
+  crond -h
+  crond -v
+  crond "* * * * * *" "ls -l"
+  crond "* * * * * *" ls -l
+  crond "* * * * * *" ./sample.bat
+```
+
+As shown above, you can pass cron expression along with script or command that need to be scheduled for recurrence.
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const cron = require('node-cron');
+const childProcess = require('node:child_process');
+const util = require('node:util');
+const exec = util.promisify(childProcess.exec);
+const minimist = require('minimist');
+const packageJson = require('./package.json');
+
+async function spawn(command) {
+    const { stdout, stderr } = await exec(command);
+    console.log(stdout);
+    console.error(stderr);
+}
+
+const args = process.argv.slice(2);
+const argv = minimist(args);
+
+if (argv.h || argv.help) {
+    console.log(`
+ Cron daemon (crond)
+
+ Syntax:
+  crond [option]
+  crond [expression] [command]
+
+ Options:
+  -h  --help        Show help menu
+  -v  --version     Show version info
+
+ Usage:
+  crond -h
+  crond -v
+  crond "* * * * * *" "ls -l"
+  crond "* * * * * *" ls -l
+  crond "* * * * * *" ./sample.bat
+  `);
+} else if (argv.v || argv.version) {
+    let appVer = packageJson.version;
+    console.log(`Version ${appVer}`);
+} else if (args.length >= 2) {
+    let argList = [...args];
+    let expression = argList.shift();
+    let command = argList.join(' ');
+    cron.schedule(expression, async () => await spawn(command));
+} else {
+    console.log('Error: Missing parameters');
+}
